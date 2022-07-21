fde54e1c9fcf8846c023e7441628bfa60ec73433
kenzing31 <kentreyes238@gmail.com>
2022-07-21 11:39:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+
+
+app.get('/', function(req, res){
+ res.send('Hello world!!!!');
+});
+app.get('/greet/:name',function(req, res){
+res.send({greeting: `Hello ${req.params.name}`})
+});
+app.listen(3000,function(){
+    console.log('Server started on port 3000...');
+});
